864e659c808359a1cbe65ef4a73f398dae775d8c
Ryna Truong <rynatruong@outlook.com>
2020-05-05 14:47:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,13 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(str)
+  if str == str.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif str == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
