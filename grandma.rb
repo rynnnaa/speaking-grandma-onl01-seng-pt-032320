62983f9d8bcff4d1c7dc8cb5623311654917ac21
Ryna Truong <rynatruong@outlook.com>
2020-05-05 14:48:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(str)
   if str == str.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif str == "I LOVE YOU TGRANDMA!"
+  elsif str == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
